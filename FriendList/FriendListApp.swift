830630fb69bc5f95f053ef63c27ffe43a708180c
Edwin Cardenas <eacardenase@gmail.com>
2026-05-11 18:06:24 -0500

    Initial Commit

---
+++ FriendList/FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListApp.swift
+//  FriendList
+//
+//  Created by Edwin Cardenas on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
